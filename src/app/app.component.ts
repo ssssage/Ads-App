a0f8e97c8d1b65508d008dde0197a3b01cbe829f
Sageer <EH@hotmail.com>
2023-03-27 02:56:39 +0000

    api fetched and ssl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'myapp';
+export class AppComponent implements OnInit{
+
+  // data: string;
+  title = "ElixirHand";
+  // myNumber: number;
+  // message: string;
+  products: any;
+  
+ 
+  constructor(private http: HttpClient){
+    // this.message = "Hello, World";
+    // this.myNumber = 5 + 6;
+    // this.data = "ElixirHand";
+    
+  }
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products').subscribe({
+      next: response => this.products = response,
+      error: error => console.log(error),
+      complete: () => console.log('Request has completed')
+      
+    })
+  }
+  
 }
